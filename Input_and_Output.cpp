c172c7d8c96843a72c5ff895311a8f03dc7c3e7f
Hassan Ibrahim <hasssanibrahiim1@gmail.com>
2024-09-10 13:17:34 +0300

    Initial Commit

---
+++ Input_and_Output.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    int x,y,z;
+    cin >> x >> y >> z;
+    int result = x + y + z ;
+    cout << result << endl ;
+     
+    return 0;
+}
+
