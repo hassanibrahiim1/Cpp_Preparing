2563b3269de819c3000a2cb58d1f8961f5d07097
Hassan Ibrahim <hasssanibrahiim1@gmail.com>
2024-09-10 15:30:19 +0300

    new file added

---
+++ Basic_Data_Types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() 
+{
+    int i ;
+    long lon ;
+    char c ;
+    float fl ; 
+    double db ;
+    scanf("%d %ld %c %f %lf", &i, &lon, &c, &fl, &db) ;
+    printf("%d\n%ld\n%c\n%f\n%lf", i, lon, c, fl, db) ; 
+    return 0;
+}
